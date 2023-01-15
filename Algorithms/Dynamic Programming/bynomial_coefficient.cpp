18757179d3129698688a1d5f16a56b556c521f64
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2023-01-15 20:04:58 +0200

    Add first folders and algos

---
+++ Algorithms/Dynamic Programming/bynomial_coefficient.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binomial_coefficient(int n, int m)
+{
+    vector<vector<int>> vec(n + 1);
+    
+    for (int i = 0; i < n + 1; i++)
+    {
+        vector<int> v1(n + 1);
+        v1[0] = 1;
+        vec[i] = v1;
+        vec[i][i] = 1;
+    }
+
+    for (int i = 2; i < n + 1; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            vec[i][j] = vec[i - 1][j - 1] + vec[i - 1][j];
+        }
+    }
+
+    return vec[n][m];
+}
+
+int main(){
+    int result = binomial_coefficient(4, 2);
+    cout << result;
+    return 0;
+}
---
+++ Problems/Leetcode/Easy/13_Roman_to_integer.cpp
@@ -0,0 +1,21 @@
+﻿#include <map>
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int result = 0;
+        map<char, int> letters = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}};
+        
+        for (int i = 0; i < s.size(); i++){
+            if (letters[s[i]] >= letters[s[i+1]]){
+                result += letters[s[i]];
+            }
+            else{
+                result -= letters[s[i]];
+            }
+        }
+
+        return result;
+    }
+};
