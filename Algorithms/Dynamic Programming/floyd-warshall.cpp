13e100442dddad51b896f7e502b71bac5ac34ba3
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2023-01-15 23:29:05 +0200

    Add Floyd-Warshall's algorithm

--- Algorithms/Dynamic Programming/floyd-warshall.cpp
+++ Algorithms/Dynamic Programming/floyd-warshall.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-vector<vector<int>> warshall(vector<vector<int>> adj){
+vector<vector<int>> floyd_warshall(vector<vector<int>> adj){
     vector<vector<int>> result = adj;
 
     for (int k = 0; k < adj.size(); k++){
         for (int i = 0; i < adj.size(); i++){
             for (int j = 0; j < adj[i].size(); j++){
-                result[i][j] |= result[i][k] & result[k][j];
+                result[i][j] = min(result[i][j], result[i][k] + result[k][j]);
             }
         }
     }
@@ -19,12 +19,13 @@ vector<vector<int>> warshall(vector<vector<int>> adj){
 }
 
 int main(){
+    const int INF = int(1e6);
     vector<vector<int>> adj{ 
-        {0, 1, 0, 0}, 
-        {0, 0, 0, 1}, 
-        {0, 0, 0, 0}, 
-        {1, 0, 1, 0}};
-    vector<vector<int>> result = warshall(adj);
+        {0, INF, 3, INF}, 
+        {2, 0, INF, INF}, 
+        {INF, 7, 0, 1}, 
+        {6, INF, INF, 0}};
+    vector<vector<int>> result = floyd_warshall(adj);
 
     for (int i = 0; i < result.size(); i++){
         for (int j = 0; j < result[i].size(); j++){
